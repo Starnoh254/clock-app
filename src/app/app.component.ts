05d379b943a2cebc9160f303a45540e3d5a6b143
Starnoh John <130779431+Starnoh254@users.noreply.github.com>
2025-02-12 18:41:36 +0300

    First Commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit , OnDestroy {
   title = 'my-clock';
+  currentTime : Date = new Date();
+  private timer : any ;
+
+  ngOnInit(): void {
+      this.timer = setInterval(() => {
+        this.currentTime = new Date();
+      } , 1000)
+  }
+
+  ngOnDestroy() {
+    if (this.timer) {
+      clearInterval(this.timer); // Cleanup when component is destroyed
+    }
+  }
 }
